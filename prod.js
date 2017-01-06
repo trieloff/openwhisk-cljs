9c8de5c7b295b5c4092869e0e54cc57f6eff72b5
Lars Trieloff <lars@trieloff.net>
2017-01-06 18:43:25 +0100

    It runs locally, now

--- prod.js
+++ prod.js
@@ -1,6 +1,8 @@
+var openwhisk_cljs = require("./target/js/openwhisk_cljs.core.js");
+
 function main(params) {
   console.log("About to call ClojureScript. Wish me luck.");
-  return module.exports.greet(params);
+  return openwhisk_cljs.greet(params);
 }
 
 console.log(main(null));
