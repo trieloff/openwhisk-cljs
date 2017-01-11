96d20fc19763579e82b183078f0fb36c5f21e35b
Lars Trieloff <lars@trieloff.net>
2017-01-11 13:40:20 +0100

    More debug output

--- src/cljs/openwhisk_cljs/core.cljs
+++ src/cljs/openwhisk_cljs/core.cljs
@@ -1,5 +1,6 @@
 (ns ^:figwheel-always openwhisk-cljs.core
   (:require [cljs.nodejs :as nodejs]
+      [cljs.pprint :as pprint]
       [cljs.core :as core]))
 (nodejs/enable-util-print!)
 
@@ -7,11 +8,15 @@
 (def -main (fn [] (str "Clojure" "Script")))
 
 (defn greet [params]
+  (println params)
+  (println (js-keys params))
+  (println (core/js->clj params))
+  ;;(println (keys (core/js->clj params)))
   {:payload (str "Hello from " "Clojure" "Script")
-   :message (str (:whorocks params) " rocks")
+   :message (str (get params "whorocks" "nobody") " rocks")
    :echo params})
   
 (set! *main-cli-fn* -main)
 
 (set! (.-exports js/module) #js {:hello -main
-                                 :greet (fn [params] (core/clj->js (greet (core/js->clj params)))) })
+                                 :greet (fn [params] (core/clj->js (greet params))) })
