ed0a6632a1e9805b6d85e610589b63d77d6851ac
Lars Trieloff <lars@trieloff.net>
2017-01-12 22:49:54 +0100

    make a HTTP request

--- src/cljs/openwhisk_cljs/core.cljs
+++ src/cljs/openwhisk_cljs/core.cljs
@@ -1,26 +1,44 @@
 (ns ^:figwheel-always openwhisk-cljs.core
   (:require [cljs.nodejs :as nodejs]
       [cljs.pprint :as pprint]
+      [httpurr.client :as http]
+      [httpurr.client.node :refer [client]]
+      [promesa.core :as p]
       [cljs.core :as core]))
 (nodejs/enable-util-print!)
 
 ;;(println "Hello from the Node!")
 (def -main (fn [] (str "Clojure" "Script")))
 
+;; {:payload (str "Hello from " "Clojure" "Script")
+;;                                                                      :http %
+;;                                                                      :message (if (= (:whorocks params) "you") "you. you rock!"
+;;                                                                                 (str (:whorocks params) " rocks"))
+;;                                                                      :echo params}
+
 (defn greet [params]
-  (println "Got these params…")
-  (println params)
-  {:payload (str "Hello from " "Clojure" "Script")
-   :message (if (= (:whorocks params) "you") "you. you rock!"
-    (str (:whorocks params) " rocks"))
-   :echo params})
+  (->> (http/get client "http://www.example.com")
+       (p/map :headers))
+  ;;(identity {:hans true})
+  )
+
+;;(p/promise (fn [resolve reject] (resolve (core/clj->js params))))
 
 (defn jswrap [func]
   (fn [p]
     (def nparams (core/js->clj p :keywordize-keys true))
     (core/clj->js (func nparams))))
 
-(def greetjs (jswrap greet))
+(defn jswrapp [func]
+  (fn [p]
+    (def nparams (core/js->clj p :keywordize-keys true))
+    (def result (func nparams))
+    ;;there is probably a better way to determine if we are getting a promise as result.
+    (if (= "[object Promise]" (type->str result))
+      (p/map core/clj->js result)
+      (core/clj->js result))))
+
+(def greetjs (jswrapp greet))
 
 (set! *main-cli-fn* -main)
 
