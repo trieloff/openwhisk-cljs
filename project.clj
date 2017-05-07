f212e340d314f15e4200db56230427f01844d5fc
Lars Trieloff <lars@trieloff.net>
2017-05-07 17:20:11 +0200

    Build with leiningen

--- project.clj
+++ project.clj
@@ -1,13 +1,14 @@
 (defproject openwhisk-cljs "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojurescript "1.9.521"]]
-  :plugins [[lein-cljsbuild "1.1.5"]]
+  :dependencies [[org.clojure/clojurescript "1.9.521" :exclusions [org.apache.ant/ant]]
+                 [org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-cljsbuild "1.1.6"]]
   :clean-targets ^{:protect false} ["target"]
   :cljsbuild {
     :builds [
     {:id "server-prod"
               :source-paths ["src/cljs"]
               :compiler {:main openwhisk-cljs.core
-                         :output-to "target/js/openwhisk_cljs.core.js"
+                         :output-to "main.js"
                          :target :nodejs
                          :optimizations :simple ;; notice this!
                          }}]})
