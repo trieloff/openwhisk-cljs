401a7de66f629caa90ae5348e1260589a9472530
Lars Trieloff <lars@trieloff.net>
2017-05-21 22:04:19 +0200

    Use WIP version of httpurr

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject openwhisk-cljs "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojurescript "1.9.521" :exclusions [org.apache.ant/ant]]
-                 [funcool/httpurr "0.6.2"]
+                 [funcool/httpurr "0.6.3"]
                  [hiccups "0.3.0"]
                  [org.clojure/clojure "1.8.0"]]
   :plugins [[lein-cljsbuild "1.1.6"]]
