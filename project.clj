7b4c8c954b2e7f09fb5f253442defdf3b100bdfa
Lars Trieloff <lars@trieloff.net>
2017-05-22 21:17:29 +0200

    Use jitpack to build

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject openwhisk-cljs "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojurescript "1.9.521" :exclusions [org.apache.ant/ant]]
                  [funcool/httpurr "0.6.3"]
+                 [com.github.trieloff/httpurr "v0.6.3-with-chunking-fix"]
                  [hiccups "0.3.0"]
                  [org.clojure/clojure "1.8.0"]]
   :plugins [[lein-cljsbuild "1.1.6"]]
+  :repositories [["jitpack" "https://jitpack.io"]]
   :clean-targets ^{:protect false} ["target"]
   :cljsbuild {
     :builds [
