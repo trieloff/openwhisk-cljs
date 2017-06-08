ad83a06a1096605614cbc1320f20cd0b7d3b40df
Lars Trieloff <lars@trieloff.net>
2017-06-08 16:49:21 -0700

    try different hashbang

--- project.clj
+++ project.clj
@@ -12,7 +12,7 @@
               :source-paths ["src/cljs"]
               :compiler {:main openwhisk-cljs.core
                          :output-to "main.js"
-                         :target :nodejs
                          :hashbang false
+                         :target :nodejs
                          :optimizations :simple ;; notice this!
                          }}]})
