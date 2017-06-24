99596a437fc8dfeec87e6ff49ac9833ecca188f9
Lars Trieloff <lars@trieloff.net>
2017-06-24 21:10:32 +0200

    q

--- src/cljs/openwhisk_cljs/core.cljs
+++ src/cljs/openwhisk_cljs/core.cljs
@@ -162,7 +162,7 @@
             questionid (nth id 1)
             answerid (nth id 2 false)]
         (cond
-          questionid "question"
+          questionid {:q "q"}
           :else (p/promise {:hello      "world"
                      :questionid questionid
                      :answerid   answerid
