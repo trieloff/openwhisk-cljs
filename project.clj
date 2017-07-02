1653ab082b5ec43138118eee73cfa7343360bfa3
Lars Trieloff <lars@trieloff.net>
2017-07-02 14:38:31 +0200

    renamed project

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject openwhisk-cljs "0.1.0-SNAPSHOT"
+(defproject stackoverflow-embed "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojurescript "1.9.521" :exclusions [org.apache.ant/ant]]
                  [funcool/httpurr "1.0.0"]
                  [hiccups "0.3.0"]
                  [com.taoensso/timbre "4.10.0"]
                  [org.clojure/clojure "1.8.0"]]
   :plugins [[lein-cljsbuild "1.1.6"]]
-  :clean-targets ^{:protect false} ["target"]
+  :clean-targets ^{:protect false} ["target", "main.js"]
   :cljsbuild {
     :builds [{:id "server-prod"
               :source-paths ["src/cljs"]
-              :compiler {:main openwhisk-cljs.core
+              :compiler {:main stackoverflow-embed.core
                          :output-to "main.js"
                          :hashbang false
                          :optimizations :simple ;; notice this!
