4194f00930dc815ad25a643830856c87831b8961
Lars Trieloff <lars@trieloff.net>
2017-07-02 14:32:12 +0200

    formatting

--- project.clj
+++ project.clj
@@ -7,12 +7,10 @@
   :plugins [[lein-cljsbuild "1.1.6"]]
   :clean-targets ^{:protect false} ["target"]
   :cljsbuild {
-    :builds [
-    {:id "server-prod"
+    :builds [{:id "server-prod"
               :source-paths ["src/cljs"]
               :compiler {:main openwhisk-cljs.core
                          :output-to "main.js"
                          :hashbang false
-                         :target :nodejs
                          :optimizations :simple ;; notice this!
-                         }}]})
+                         :target :nodejs }}]})
